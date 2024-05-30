31e5c15ae26480019b2ced6fa0e7c05702677cd4
chitangchin <chitangb.chin@gmail.com>
2024-05-30 16:42:26 -0400

    TwoSum

---
+++ week 1/README.md
@@ -0,0 +1,22 @@
+
+
+
+TwoSum:
+
+The idea is Fast search look up algorithm
+
+Using cache memory for fast retrieval n
+
+You can also use two pointer algorithm to cut the workload to n/2
+
+Clarifying Questions: 
+1. Empty List
+2. Different Data Type instead of Array?
+3. Maximum/Minimum amount of elements in the List
+4. What is the number type of the element in the Array? 
+5. Maximum/Minimum Range of each element
+6. How many solutions can there be?
+7. Handling No solutions
+
+Pattern:
+Utilizes Hashing Data Structure with a search algortithm to find the solution.
--- week 1/twoSum.js
+++ week 1/twoSum.js
@@ -1,3 +1,24 @@
+/* 
+Clarifying Questions: How many solutions can there be?
+What if User enteres an empty list?
+What is the max elements in the list?
+What is the range for the elements?
+Can there be 0 solutions?
+Can the target be a rational number? or does it have to be a natural number?
+*/
+
+/*
+Understanding the question:
+Two pointer Question
+In real life situation this is used great for searching in large datasets
+
+The idea is divide and conquer one pointer on top and one pointer on the bottom.
+
+we create a cache to store previous numbers within nums. that way if we find a number and a number exists in hash such that it adss up to the target.
+then we stop the function at that point
+
+ */
+
 function twoSum(nums, target) {
     //Exactly one solution
     //Cannot use the same element twice
